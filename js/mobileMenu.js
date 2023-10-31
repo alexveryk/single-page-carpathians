8767d209b706246730ebb8acdace26d459ab6016
alexveryk <veryk.av@gmail.com>
2023-11-01 00:10:04 +0200

    update mobile menu

---
+++ js/mobileMenu.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    hide: document.querySelectorAll("[data-hide]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
+    refs.hide.forEach((element) => {
+      element.classList.toggle("hide-on-mobile-menu");
+    });
+  }
+})();

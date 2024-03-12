dee469c9fad05756ada172e4f3ff4b76db284e03
Oleksandr Veryk <veryk.av@gmail.com>
2024-03-13 00:13:31 +0200

    Create flipCard.js

---
+++ js/flipCard.js
@@ -0,0 +1,14 @@
+function flipCardOne() {
+    const card = document.querySelector("#reviews__card-one");
+    card.classList.toggle("active");
+  }
+
+  function flipCardTwo() {
+    const card = document.querySelector("#reviews__card-two");
+    card.classList.toggle("active");
+  }
+
+  function flipCardThree() {
+    const card = document.querySelector("#reviews__card-three");
+    card.classList.toggle("active");
+  }
